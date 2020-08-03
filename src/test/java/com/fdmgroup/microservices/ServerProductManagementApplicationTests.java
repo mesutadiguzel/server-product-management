c20956fbc0a0b0de33bf3fadaf0f3fa8e9d7c5d7
mesut <mesutadiguzel@gmail.com>
2020-08-03 23:16:20 +0100

    first commit

---
+++ src/test/java/com/fdmgroup/microservices/ServerProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
